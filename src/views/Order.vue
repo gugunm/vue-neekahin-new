36490a82af1da7a3304da85c1dc309fc9e23025a
aris afrianto <arisafrianto23@gmail.com>
2021-08-16 16:38:31 +0700

    pull

--- src/views/Order.vue
+++ src/views/Order.vue
@@ -1,6 +1,85 @@
 <template>
   <nav-landing />
-  <div class="h-96 bg-green-500">
+  <div class="bg-green pt-28 pb-16">
+    <div class="py-4 sm:py-5 px-6 xl:px-20">
+      <div class="grid xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-1 sm:grid-cols-1">
+        <section class="pt-4 pb-0 bg-order col-span-2 xl:px-16 xl:col-span-1  lg:col-span-1 md:px-8 xl:py-8 lg:py-8 md:py-8">
+          <div class="grid grid-cols-1 xl:grid-cols-1 lg:grid-cols-1 md:grid-cols-3">
+            <div class="col-span-3 flex flex-col items-center py-4 md:col-span-1">
+              <div class="grid grid-cols-3">
+                <div class="pb-4 xl:col-span-3 lg:col-span-3 md:col-span-3">
+                  <svg width="78" height="56" viewBox="0 0 78 56" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M10.4283 51.6125C13.8899 54.0031 17.899 55.2394 22.0585 55.2394C23.7303 55.2394 25.4278 55.0408 27.1204 54.6381C34.3025 52.9252 39.0053 49.033 42.4992 44.1447C42.7008 44.0302 42.8719 43.8644 42.9888 43.6508C45.6935 38.8156 48.2469 34.8929 51.7491 33.18C51.6524 33.3536 51.5427 33.514 51.461 33.7054C50.7495 35.4207 50.895 37.3473 51.8965 39.4275C53.273 42.2868 55.4499 43.5565 58.9564 43.5565C60.618 43.5565 62.6548 43.2839 65.5479 42.6541C68.6238 41.9915 71.1936 42.2641 75.2995 43.5029C75.4271 43.541 75.5542 43.5565 75.6818 43.5565C75.6895 43.5565 75.6997 43.5565 75.7074 43.5565C76.4416 43.5386 77.038 42.9828 77.038 42.2515C77.038 41.9944 76.9669 41.7571 76.8311 41.5478C76.0336 38.9587 70.8775 23.5498 61.1516 23.5498C59.7519 23.5498 58.3247 23.8732 56.9027 24.516C55.8346 24.9988 54.5577 26.3705 54.1705 28.0679C54.0405 28.6436 54.0608 29.1999 54.1526 29.7451C52.1676 30.0128 50.4214 30.7571 48.8588 31.8707C49.3049 30.79 49.7433 29.7045 50.1768 28.6209C50.3117 28.2941 50.4393 27.9911 50.5644 27.6702C50.6539 27.5832 50.7631 27.5194 50.8245 27.4048C55.575 18.9124 62.3831 15.822 69.9935 18.706C70.6615 18.9699 71.4208 18.6257 71.6755 17.9491C71.9302 17.2758 71.5914 16.5218 70.9186 16.2657C64.8863 13.9747 59.1666 14.9327 54.4383 18.8336C59.0593 9.08301 62.5943 3.48999 70.4624 2.81673C71.1762 2.75679 71.7113 2.12219 71.6528 1.40736C71.5914 0.690104 70.9747 0.139115 70.2405 0.21693C59.7447 1.11156 55.6127 10.0385 50.8264 20.3715C49.7409 22.7151 48.7365 25.2202 47.7626 27.647C47.5538 28.1694 47.3421 28.6919 47.1304 29.2144C46.7046 23.5687 43.7839 19.0119 38.521 16.1187C38.6331 15.6523 38.7071 15.1767 38.7124 14.6838C38.7279 12.9849 38.0802 11.3793 36.8898 10.1686C34.5065 7.74324 30.3214 7.70022 27.8898 10.0878C25.3896 12.5489 25.3562 16.5861 27.8134 19.0839C29.0193 20.3135 30.6327 20.9887 32.3528 20.9887C34.03 20.9887 35.6099 20.3415 36.8081 19.1675C37.0222 18.9583 37.1957 18.7229 37.3765 18.4909C44.0594 22.2391 46.1594 29.0148 43.3484 37.6832C39.8438 44.3917 35.0826 50.0446 26.5235 52.0939C21.3447 53.3221 16.1576 52.3892 11.9218 49.4637C7.17362 46.1858 3.98513 40.701 3.16928 34.413C3.07504 33.7016 2.40467 33.197 1.70917 33.284C0.992881 33.3782 0.488291 34.0331 0.580123 34.7465C1.4849 41.774 5.07841 47.9142 10.4283 51.6125ZM53.8646 34.7194C54.5552 33.05 56.5533 32.3183 56.5663 32.313C56.5813 32.3081 56.5895 32.2927 56.6045 32.2874C58.8017 32.456 61.33 33.2888 64.3377 34.9538C64.967 35.3105 65.762 35.0761 66.1115 34.4517C66.4609 33.8195 66.2338 33.0268 65.604 32.6726C62.2425 30.8069 59.3571 29.8867 56.8336 29.675C56.6959 29.3516 56.6374 29.0177 56.7239 28.6455C56.9153 27.7813 57.6466 27.0496 57.9805 26.8944C59.0637 26.4072 60.1318 26.1617 61.156 26.1617C67.6581 26.1617 71.9988 35.8461 73.6658 40.3574C70.3217 39.5725 67.8645 39.4879 65.01 40.1027C62.301 40.6861 60.4402 40.9408 58.967 40.9408C56.4663 40.9408 55.1898 40.2269 54.2594 38.2902C53.5896 36.9137 53.4591 35.7156 53.8646 34.7194ZM34.9758 17.3111C33.5437 18.7205 31.0764 18.6949 29.6718 17.2604C28.2219 15.7872 28.247 13.3991 29.7177 11.9539C30.4263 11.2594 31.3567 10.881 32.3432 10.881C33.3601 10.881 34.3108 11.2783 35.0241 12.0061C35.725 12.7186 36.1073 13.6654 36.1 14.6707C36.087 15.6697 35.6916 16.6103 34.9758 17.3111Z" fill="white"/>
+                  </svg>
+                </div>
+
+                <div class="pb-4 flex flex-col items-left text-white xl:col-span-3 xl:items-center lg:items-center lg:xl:col-span-3 md:items-center md:col-span-3 xs:text-left">
+                  <p>Silver</p>
+                  <span>IDR 250.000</span>
+                </div>
+              </div>
+            </div>
+            <div class="uppercase  p-8 py-16 border-dashed hidden rounded-2xl box-content border-white text-white xl:col-span-3 xl:border xl:block lg:border md:col-span-2 md:border md:block">
+              <div class="grid grid-cols-2">
+                <div class="col-span-2 xl:col-span-2 lg:col-span-2 md:col-span-1">
+                  <ul class="list-dist-inside leading-12">
+                    <li><span>&#9679; </span>Simple design</li>
+                    <li><span>&#9679; </span>Max 5 foto/galeri</li>
+                    <li><span>&#9679; </span>Maps</li>
+                  </ul>
+                </div>
+                <div class="col-span-2 xl:col-span-2 lg:col-span-2 md:col-span-1">
+                  <ul class="list-dist-inside leading-12">
+                    <li><span>&#9679; </span>Countdown</li>
+                    <li><span>&#9679; </span>Music</li>
+                    <li><span>&#9679; </span>Link / Url Domain</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+        </section>
+
+        <section class="col-span-2 pt-12 pb-8 xl:col-span-2 px-4 border-color capitalize">
+          <p class="pb-4 px-4 text-xl">lengkapi data diri</p>
+          <div class=" capitalize">
+            <form class="grid grid-cols-2" action="" method="post">
+              <div class="pb-4 block px-4 col-span-2 xl:col-span-1 lg:col-span-1 md:col-span-1">
+                <label class="block text-left mb-2">nama mempelai pria</label>
+                <input name="nama-pria" type="text" class="px-5 block w-full border-color-2" placeholder="Ex. Agus">
+              </div>
+
+              <div class="pb-4 block px-4 col-span-2 xl:col-span-1 lg:col-span-1 md:col-span-1">
+                <label class="block text-left mb-2">nama mempelai wanita</label>
+                <input name="nama-wanita" type="text" class="px-5 block w-full border-color-2" placeholder="Ex. Ayu">
+              </div>
+              <div class="pb-4 block px-4 col-span-2 xl:col-span-1 lg:col-span-1 md:col-span-1">
+                  <label class="block text-left mb-2">email</label>
+                  <input name="email" type="text" class="px-5 block w-full border-color-2" placeholder="agus@email.com">
+              </div>
+              <div class="pb-4 block px-4 col-span-2 xl:col-span-1 lg:col-span-1 md:col-span-1">
+                  <label class="block text-left mb-2">no. whatsapp</label>
+                  <input type="text" class="px-5 block w-full border-color-2" placeholder="08xxxxxxxxxx">
+              </div>
+              <div class="pb-24 px-4 col-span-2">
+                <label class="block text-left mb-2">request</label>
+                <textarea name="request" id="" class="px-5 w-full h-48 border-color-2" placeholder="*Saya inging yang simple dan modern"></textarea>
+              </div>
+
+              <div class="px-4 col-span-2 xl:col-span-1 lg:col-span-1 md:col-span-1">
+                <button type="submit" class="w-full xl:w-8/12 button-color px-20 py-2 capitalize">pesan undangan</button>
+              </div>
+
+
+
+
+            </form>
+          </div>
+        </section>
+      </div>
+    </div>
   </div>
   <footer-landing />
 </template>
@@ -8,7 +87,44 @@
 <script>
 import FooterLanding from './parts/FooterLanding.vue';
 import NavLanding from './parts/NavLanding.vue';
+import silverPng from '@/assets/img/vector-silver.png';
 export default {
   components: { NavLanding, FooterLanding },
+  data() {
+    return {
+      silverPng,
+    };
+  },
 };
+
+
 </script>
+<style scoped>
+  .card-order{
+    background-color: hsla(0,0%,100%,.75);
+  }
+  .bg-order{
+    background-color:#B58200;
+  }
+  .border-color{
+    border: #B58200 solid 1px;
+  }
+  .button-color{
+    background-color: #B58200;
+    color:#ffffff;
+    text-size:12px;
+    border-radius: 5px;
+  }
+  .border-color-2{
+    border: #C4C4C4 1px solid;
+    border-radius: 5px;
+  }
+  .list-dist-inside span{
+    /* list-style: inside; */
+    font-size: 8px;
+    color: #8A5D1A;
+    padding-right: 1rem;
+    vertical-align: middle;
+  }
+
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,25 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+
+    borderWidth: {
+      DEFAULT: '1px',
+      '0': '0',
+      '2': '2px',
+      '3': '3px',
+      '4': '4px',
+      '6': '6px',
+      '8': '8px',
+    },
+    screens: {
+      xs: '475px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+
+      // orderXl: '346px',
+    },
     extend: {
       zIndex: {
         '-1': '-1'
@@ -18,12 +37,32 @@ module.exports = {
       },
       transitionProperty: {
         position: 'right, left, top, bottom, margin, padding'
-      }
+      },
+      maxWidth: {
+        '0': '0',
+        '1/4': '25%',
+        '1/2': '50%',
+        '3/4': '75%',
+        'full': '100%',
+       },
+       minWidth: {
+        '0': '0',
+        '1/4': '25%',
+        '1/2': '50%',
+        '3/4': '75%',
+        'full': '100%',
+       },
+       lineHeight: {
+        'extra-loose': '2.5',
+        '12': '3rem',
+       }
     },
+
   },
   variants: {
     extend: {
       margin: ['last'],
+      minWidth: ['responsive', 'hover', 'focus'],
     }
   },
   plugins: [
